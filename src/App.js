e8ca7830ab444e7711444ad4b96f49a297634111
Rizky Alfikri Rachmat <rizky.rachmat@dana.id>
2021-12-03 21:36:19 +0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import './App.css';
 import React from 'react';
 import CounterComponent from './CounterComponent';
+import {
+  Routes,
+  Route,
+  NavLink,
+  Navigate
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
 
 // context usage
 export let TemaSitus = React.createContext('dark')
 
+
 function App() {
-
-  // const alert = () => window.alert(123)
-
-  // const ubah = (obj) => {
-  //   const btn = obj.target
-  //   btn.innerText = 'Clicked'
-  // }
-
-  // const Event = () => {
-  //   return <button onClick={ubah}>Info</button>
-  // }
-
-  // function onCustomClick() {
-  //   alert('Yahahah')
-  // }
 
   return (
     <div className="App">
       <TemaSitus.Provider value={'blue'} >
-        <header className="App-header">
-          < CounterComponent />
-        </header>
+        <ul className='menu'>
+          <li>
+            <NavLink to='/' activeClassName='active' >Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' activeClassName='active' >About</NavLink>
+          </li>
+          <li>
+            <NavLink to='/member' activeClassName='active' >Member</NavLink>
+          </li>
+        </ul>
+
+        <div className='main'>
+          <Routes >
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/member' element={<Navigate to='/'/>} />
+          </Routes>
+        </div>
       </TemaSitus.Provider>
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Home = () => <h2>Halaman Home</h2>
+
+
+export default Home
